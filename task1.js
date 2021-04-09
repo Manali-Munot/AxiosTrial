1d8a61d9bbfefe12dfc15312df735a521e718c89
Manali-Munot <manali@techprescient.com>
2021-04-09 11:53:36 +0530

    rendering trial

--- task1.js
+++ task1.js
@@ -1,9 +1,21 @@
 "use strict";
+var arr;
+arr = [];
 function fetchData() {
-    fetch('https://cat-fact.herokuapp.com/facts')
+    fetch('https://reqres.in/api/users')
         .then(function (response) { return response.json(); })
         .then(function (data) {
-        console.log(data);
+        console.log(data.data);
+        for (var i = 0; i < data.data.length; i++) {
+            var x = "Name : " + data.data[i].first_name + " " + data.data[i].last_name;
+            arr[i] = x;
+            console.log(x);
+            var disp = document.querySelector("#display");
+            var h4 = document.createElement('h4');
+            h4.innerText = x;
+            disp === null || disp === void 0 ? void 0 : disp.append(h4);
+        }
+        console.log(arr);
     });
 }
 fetchData();
--- task1.ts
+++ task1.ts
@@ -1,13 +1,26 @@
-
+let arr : String[];
+arr=[];
 
 function fetchData()
 {
-    fetch('https://cat-fact.herokuapp.com/facts')   
+    fetch('https://reqres.in/api/users')   
     .then(response => {return response.json();})   
     .then(data => 
-      
-      {
-    console.log(data);
+     {
+    console.log(data.data);
+
+    for(let i=0;i<data.data.length;i++)
+    {
+          let x="Name : "+data.data[i].first_name +" "+ data.data[i].last_name;
+          arr[i]=x;
+          console.log(x);
+          let disp=document.querySelector("#display");
+          let h4 = document.createElement('h4');
+          h4.innerText =x;
+          disp?.append(h4);
+    }
+    console.log(arr);
+    
     });
   } 
 fetchData();
